9bb1cd0064c4c5c6a8d2f93ebdb7ba72e5ff4ca9
unknown <DAM108@iesataulfo.local>
2024-04-16 15:31:39 +0200

    Segunda fase del proyecto (Clase1 creada y Principal creada y comentada)

--- src/directorioTrabajoGabasSabrina/Principal.java
+++ src/directorioTrabajoGabasSabrina/Principal.java
@@ -1,9 +1,18 @@
 package directorioTrabajoGabasSabrina;
 
 public class Principal {
+	
+	/**
+	 * @param args
+	 * @version 1.0
+	 * @author Sabrina Gabas Jimenez
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// En esta clase vamos a hacer una instancia de un objeto clase1 
+		
 
 	}
 
